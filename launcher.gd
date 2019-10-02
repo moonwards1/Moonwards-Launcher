41953e9ce34e5dc022a897a83e7bb249a3679966
Gyrth <gyrthmcmulin@gmail.com>
2019-10-03 00:24:26 +0200

    Added platform checks.

--- launcher.gd
+++ launcher.gd
@@ -1,5 +1,12 @@
 extends Node
 
+enum PLATFORMS {
+	NONE
+	LINUX,
+	OSX,
+	WINDOWS
+	}
+
 onready var http_request : HTTPRequest = $HTTPRequest
 onready var text_log = $TextLog
 onready var progress_bar = $ProgressBar
@@ -12,16 +19,18 @@ var download_queue : Array = []
 var downloads_done : Array = []
 var file : File = File.new()
 var directory : Directory = Directory.new()
+var platform : int = PLATFORMS.NONE
 
 signal receive_update_message
 
-func _ready():
+func _ready() -> void:
 	launch_button.hide()
 	update_button.hide()
+	_platform_check()
 	_check_for_updates()
 	set_process(false)
 
-func _process(delta):
+func _process(delta : float) -> void:
 	if download_queue.size() != 0:
 		var body_size = http_request.get_body_size()
 		var downloaded_bytes = http_request.get_downloaded_bytes()
@@ -33,11 +42,20 @@ func _process(delta):
 		var new_value = percent_done + (percent_per_download * (percent_current_file / 100.0))
 		progress_bar.value = new_value
 
-func _log(var text):
+func _platform_check() -> void:
+	var platform_name = OS.get_name()
+	if platform_name == "X11":
+		platform = PLATFORMS.LINUX
+	elif platform_name == "Windows":
+		platform = PLATFORMS.WINDOWS
+	elif platform_name == "OSX":
+		platform = PLATFORMS.OSX
+
+func _log(text : String) -> void:
 	emit_signal("receive_update_message", text)
 	text_log.text += text + "\n"
 
-func _receive_file(result, response_code, headers, body):
+func _receive_file(result : int, response_code : int, headers : PoolStringArray, body : PoolByteArray) -> void:
 	_log("result " + str(response_code) + ".")
 	
 	if result != HTTPRequest.RESULT_SUCCESS:
@@ -59,7 +77,7 @@ func _receive_file(result, response_code, headers, body):
 	download_queue.remove(0)
 	_get_next_update()
 
-func _get_next_update():
+func _get_next_update() -> void:
 	if download_queue.size() != 0:
 		_log("Start download of file: " + download_queue[0])
 		_set_status("Downloading...")
@@ -77,7 +95,7 @@ func _get_next_update():
 func _set_status(text : String):
 	status.text = text
 
-func _check_for_updates():
+func _check_for_updates() -> void:
 	http_request.connect("request_completed", self, "_receive_md5_json")
 	var error = http_request.request(server_url + "md5_list.json")
 	
@@ -86,7 +104,7 @@ func _check_for_updates():
 		_log("Error retrieving update list!")
 		return
 
-func _receive_md5_json(result, response_code, headers, body):
+func _receive_md5_json(result : int, response_code : int, headers : PoolStringArray, body : PoolByteArray) -> void:
 	if result != HTTPRequest.RESULT_SUCCESS:
 		_set_status("Error")
 		_log("An error occured getting the update list!")
@@ -99,7 +117,19 @@ func _receive_md5_json(result, response_code, headers, body):
 		_log(json.error_string)
 		return
 	
-	for file_data in json.result["files"]:
+	var json_result = json.result
+	var files
+	if platform == PLATFORMS.LINUX:
+		files = json_result.X11["files"]
+	elif platform == PLATFORMS.WINDOWS:
+		files = json_result.Windows["files"]
+	elif platform == PLATFORMS.OSX:
+		files = json_result.OSX["files"]
+	else:
+		_set_status("Platform not supported.")
+		return
+	
+	for file_data in files:
 		var file_name = file_data["file"]
 		var file_md5 = file_data["md5"]
 		
@@ -120,12 +150,23 @@ func _receive_md5_json(result, response_code, headers, body):
 	
 	http_request.disconnect("request_completed", self, "_receive_md5_json")
 
-func _start_update():
+func _start_update() -> void:
 	update_button.hide()
 	_get_next_update()
 	set_process(true)
 
-func _launch_moonwards():
+func _launch_moonwards() -> void:
 	var output = []
-	OS.execute('/bin/sh', ["-c", "cd " + OS.get_user_data_dir() + " && ./MoonTown.x86_64"], false, output)
-	get_tree().quit()
+	var pid
+	
+	if platform == PLATFORMS.LINUX:
+		pid = OS.execute('/bin/sh', ["-c", "cd " + OS.get_user_data_dir() + " && ./MoonTown.x86_64"], false, output)
+	elif platform == PLATFORMS.OSX:
+		pid = OS.execute('/bin/sh', ["-c", "cd " + OS.get_user_data_dir() + " && ./MoonTown.app"], false, output)
+	elif platform == PLATFORMS.WINDOWS:
+		pid = OS.execute('CMD.exe', ["/C", "cd " + OS.get_user_data_dir() + " && ./MoonTown.exe"], false, output)
+	
+	if pid == -1:
+		_set_status("Error launching Moonwards : " + str(pid))
+	else:
+		get_tree().quit()
