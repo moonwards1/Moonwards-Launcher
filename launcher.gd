20818c2ed7143c2fac263642eb8972305e55ba4e
Gyrth <gyrthmcmulin@gmail.com>
2019-10-03 04:16:18 +0200

    Made progress with launcher updating itself.

--- launcher.gd
+++ launcher.gd
@@ -7,12 +7,19 @@ enum PLATFORMS {
 	WINDOWS
 	}
 
+enum UPDATE_STATE{
+	NONE,
+	LAUNCHER,
+	MOONWARDS
+	}
+
 onready var http_request : HTTPRequest = $HTTPRequest
 onready var text_log = $TextLog
 onready var progress_bar = $ProgressBar
 onready var status = $HBoxContainer/VBoxContainer/Status
 onready var launch_button = $HBoxContainer/VBoxContainer/LaunchButton
-onready var update_button = $HBoxContainer/VBoxContainer/UpdateButton
+onready var update_moonwards_button = $HBoxContainer/VBoxContainer/UpdateMoonwardsButton
+onready var update_launcher_button = $HBoxContainer/VBoxContainer/UpdateLauncherButton
 
 var server_url : String = "http://107.173.129.154/moonwards/"
 var download_queue : Array = []
@@ -20,15 +27,104 @@ var downloads_done : Array = []
 var file : File = File.new()
 var directory : Directory = Directory.new()
 var platform : int = PLATFORMS.NONE
+var update_state : int = UPDATE_STATE.NONE
+var update_state_name : String = "NA"
 
 signal receive_update_message
 
 func _ready() -> void:
 	launch_button.hide()
-	update_button.hide()
-	_platform_check()
-	_check_for_updates()
+	update_moonwards_button.hide()
+	update_launcher_button.hide()
 	set_process(false)
+	
+	_platform_check()
+	_check_for_launcher_updates()
+
+func _check_for_launcher_updates() -> void:
+	http_request.connect("request_completed", self, "_receive_md5_json")
+	var error = http_request.request(server_url + "launcher_md5.json")
+	
+	update_state = UPDATE_STATE.LAUNCHER
+	update_state_name = "launcher"
+	
+	if error != OK:
+		_set_status("Error getting launcher updates.")
+		return
+
+func _receive_md5_json(result : int, response_code : int, headers : PoolStringArray, body : PoolByteArray) -> void:
+	if result != HTTPRequest.RESULT_SUCCESS:
+		_set_status("Error, could not fetch " + update_state_name + " launcher updates.")
+		_log("Error status " + str(result))
+		return
+	
+	var json = JSON.parse(body.get_string_from_utf8())
+	if json.error != OK:
+		_set_status("Error parsing " + update_state_name + " update list.")
+		_log(json.error_string)
+		return
+	
+	var json_result = json.result
+	var path
+	var files
+	if platform == PLATFORMS.LINUX:
+		files = json_result.X11["files"]
+		path = json_result.X11["path"]
+	elif platform == PLATFORMS.WINDOWS:
+		files = json_result.Windows["files"]
+		path = json_result.Windows["path"]
+	elif platform == PLATFORMS.OSX:
+		files = json_result.OSX["files"]
+		path = json_result.OSX["path"]
+	else:
+		_set_status("Platform not supported.")
+		return
+	
+	for file_data in files:
+		var file_name = file_data["file"]
+		var file_md5 = file_data["md5"]
+		
+		_log("File : " + file_name + " md5: " + file_md5)
+		var write_dir
+		if update_state == UPDATE_STATE.LAUNCHER:
+			write_dir = "res://"
+		elif update_state == UPDATE_STATE.MOONWARDS:
+			write_dir = "user://"
+		
+		if directory.file_exists(write_dir + file_name):
+			var local_md5 = file.get_md5(write_dir + file_name)
+			_log("Local md5: " + local_md5)
+			if local_md5 != file_md5:
+				download_queue.append({"file_name" : file_name, "path" : path})
+		else:
+			_log("File : " + file_name + " does not exist")
+			download_queue.append({"file_name" : file_name, "path" : path})
+	
+	http_request.disconnect("request_completed", self, "_receive_md5_json")
+	
+	if download_queue.size() != 0:
+		if update_state == UPDATE_STATE.LAUNCHER:
+			_set_status("Launcher Updates Available")
+			update_launcher_button.show()
+		elif update_state == UPDATE_STATE.MOONWARDS:
+			_set_status("Moonwards Updates Available")
+			update_moonwards_button.show()
+	else:
+		if update_state == UPDATE_STATE.LAUNCHER:
+			_check_for_moonwards_updates()
+		elif update_state == UPDATE_STATE.MOONWARDS:
+			launch_button.show()
+
+func _check_for_moonwards_updates() -> void:
+	http_request.connect("request_completed", self, "_receive_md5_json")
+	var error = http_request.request(server_url + "moonwards_md5.json")
+	
+	update_state = UPDATE_STATE.MOONWARDS
+	update_state_name = "Moonwards"
+	
+	if error != OK:
+		_set_status("Error getting Moonwards updates.")
+		return
 
 func _process(delta : float) -> void:
 	if download_queue.size() != 0:
@@ -56,22 +152,27 @@ func _log(text : String) -> void:
 	text_log.text += text + "\n"
 
 func _receive_file(result : int, response_code : int, headers : PoolStringArray, body : PoolByteArray) -> void:
-	_log("result " + str(response_code) + ".")
+	_log("Result : " + str(response_code) + ".")
 	
 	if result != HTTPRequest.RESULT_SUCCESS:
-		_log("An error occured getting the an update.")
+		_log("An error occured getting an update.")
 		_get_next_update()
 		return
 	
 	if response_code != HTTPClient.RESPONSE_OK:
-		_set_status("Error")
+		_set_status("Error downloading file.")
 		_log("Received response code " + str(response_code) + ".")
 	else:
-		var write_path = "user://" + download_queue[0]
-		file.open(write_path, File.WRITE)
+		var write_dir
+		if update_state == UPDATE_STATE.LAUNCHER:
+			write_dir = "res://" + download_queue[0].file_name
+		elif update_state == UPDATE_STATE.MOONWARDS:
+			write_dir = "user://" + download_queue[0].file_name
+		
+		file.open(write_dir, File.WRITE)
 		file.store_buffer(body)
 		file.close()
-		_log("Done writing " + "user://" + download_queue[0])
+		_log("Done writing " + write_dir)
 	
 	downloads_done.append(download_queue[0])
 	download_queue.remove(0)
@@ -79,81 +180,41 @@ func _receive_file(result : int, response_code : int, headers : PoolStringArray,
 
 func _get_next_update() -> void:
 	if download_queue.size() != 0:
-		_log("Start download of file: " + download_queue[0])
+		_log("Start download of file: " + download_queue[0].file_name)
 		_set_status("Downloading...")
 		
 		http_request.connect("request_completed", self, "_receive_file")
 		
-		var error = http_request.request(server_url + download_queue[0])
+		var error = http_request.request(server_url + download_queue[0].path + download_queue[0].file_name)
 		if error != OK:
 			_set_status("Error")
-			_log("Error " + str(error) + " requesting update " + server_url + download_queue[0])
+			_log("Error " + str(error) + " requesting update " + server_url + download_queue[0].path + download_queue[0].file_name)
 	else:
-		_set_status("Update done")
-		launch_button.show()
+		if update_state == UPDATE_STATE.LAUNCHER:
+			_restart_launcher()
+		elif update_state == UPDATE_STATE.MOONWARDS:
+			_set_status("Update done")
+			launch_button.show()
 
-func _set_status(text : String):
-	status.text = text
-
-func _check_for_updates() -> void:
-	http_request.connect("request_completed", self, "_receive_md5_json")
-	var error = http_request.request(server_url + "md5_list.json")
+func _restart_launcher() -> void:
+	var output = []
+	var pid
 	
-	if error != OK:
-		_set_status("Error")
-		_log("Error retrieving update list!")
-		return
-
-func _receive_md5_json(result : int, response_code : int, headers : PoolStringArray, body : PoolByteArray) -> void:
-	if result != HTTPRequest.RESULT_SUCCESS:
-		_set_status("Error")
-		_log("An error occured getting the update list!")
-		return
-	
-	var json = JSON.parse(body.get_string_from_utf8())
-	if json.error != OK:
-		_set_status("Error")
-		_log("Error parsing update list!")
-		_log(json.error_string)
-		return
-	
-	var json_result = json.result
-	var files
 	if platform == PLATFORMS.LINUX:
-		files = json_result.X11["files"]
-	elif platform == PLATFORMS.WINDOWS:
-		files = json_result.Windows["files"]
+		pid = OS.execute("./Moonwards-Launcher.x86_64", [], false, output)
 	elif platform == PLATFORMS.OSX:
-		files = json_result.OSX["files"]
+		pid = OS.execute("./Moonwards-Launcher.app", [], false, output)
+	elif platform == PLATFORMS.WINDOWS:
+		pid = OS.execute("./Moonwards-Launcher.exe", [], false, output)
+	
+	if pid == -1:
+		_set_status("Error executing " + update_state_name + " : " + str(pid))
 	else:
-		_set_status("Platform not supported.")
-		return
-	
-	for file_data in files:
-		var file_name = file_data["file"]
-		var file_md5 = file_data["md5"]
-		
-		_log("File : " + file_name + " md5: " + file_md5)
-		if directory.file_exists("user://" + file_name):
-			var local_md5 = file.get_md5("user://" + file_name)
-			if local_md5 != file_md5:
-				download_queue.append(file_name)
-		else:
-			download_queue.append(file_name)
-	
-	if download_queue.size() != 0:
-		_set_status("Updates Available")
-		update_button.show()
-	else:
-		_set_status("Ready")
-		launch_button.show()
-	
-	http_request.disconnect("request_completed", self, "_receive_md5_json")
+		get_tree().quit()
 
-func _start_update() -> void:
-	update_button.hide()
-	_get_next_update()
-	set_process(true)
+func _set_status(text : String) -> void:
+	_log(text)
+	status.text = text
 
 func _launch_moonwards() -> void:
 	var output = []
@@ -170,3 +231,13 @@ func _launch_moonwards() -> void:
 		_set_status("Error launching Moonwards : " + str(pid))
 	else:
 		get_tree().quit()
+
+func _update_launcher() -> void:
+	update_launcher_button.hide()
+	_get_next_update()
+	set_process(true)
+
+func _update_moonwards() -> void:
+	update_moonwards_button.hide()
+	_get_next_update()
+	set_process(true)
