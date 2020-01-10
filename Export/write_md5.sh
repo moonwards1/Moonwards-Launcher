7ea38bede7b650f1adb21786f155d752d9d26de5
Gyrth <gyrthmcmulin@gmail.com>
2020-01-10 22:08:15 +0100

    Fixed md5 creation script.

--- Export/write_md5.sh
+++ Export/write_md5.sh
@@ -3,7 +3,7 @@
 find . -type f -print0 | while IFS= read -r -d '' file
 do
 	extention=$( echo "$file" | rev | cut -d'.' -f 1 | rev )
-	if [ "$extention" != "md5" && "$extention" != "json" ] && "$extention" != "sh" ]; then
+	if [ "$extention" != "md5" ] && [ "$extention" != "json" ] && [ "$extention" != "sh" ]; then
 		md5sum=$(md5sum -b "$file")
 		md5=$( echo "$md5sum" | cut -d " " -f 1 )
 		echo $file
